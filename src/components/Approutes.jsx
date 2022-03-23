804acef33f120a1900dd16c70608a1a27ee4a5bf
Surya <123004250@sastra.ac.in>
2022-03-24 05:15:02 +0530

    feat: added protected routes

--- src/components/Approutes.jsx
+++ src/components/Approutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
 import {
   Home,
   ProductListing,
@@ -7,18 +7,24 @@ import {
   Signin,
   Signup
 } from "../Pages";
-
+import {useAuth} from './../contexts/AuthContext'
 const Approutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<ProductListing />} />
-      <Route path="/wishlist" element={<WishList />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route path="/" element={<ProtectedRoutes />}>
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/wishlist" element={<WishList />} />
+			</Route>
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
-
+const ProtectedRoutes=()=>{
+  const {isAuthenticated}=useAuth();
+  return isAuthenticated()?<Outlet/>:<Navigate to='signin'/>
+}
 export { Approutes };
+ 
