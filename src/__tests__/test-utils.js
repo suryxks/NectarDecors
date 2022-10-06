429650af0cadc8cf8d7e3aa8a8e7d89b2bf2c144
Surya <123004250@sastra.ac.in>
2022-10-06 07:50:17 +0530

    added custom reader function

---
+++ src/__tests__/test-utils.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import {render} from '@testing-library/react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {
+  AuthProvider,
+  DataProvider,
+  ProductsProvider,
+  CartProvider,
+  WishListProvider,
+} from '../contexts'
+
+const AllTheProviders = ({children}) => {
+  return (
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <WishListProvider>
+            <DataProvider>
+              <ProductsProvider>{children}</ProductsProvider>
+            </DataProvider>
+          </WishListProvider>
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  )
+}
+export const appRender = (ui,options) =>render(ui,{wrapper:AllTheProviders,...options})
+
+export * from '@testing-library/react';
